6e0c71b8deb2dae4671319dff678b480737f74a0
mr.liu <158442534@qq.com>
2021-09-16 14:41:00 +0800

    升级第三方库

--- lion-common-frontend-restful/src/main/java/com/lion/ApplicationCommonFrontendRestful.java
+++ lion-common-frontend-restful/src/main/java/com/lion/ApplicationCommonFrontendRestful.java
@@ -1,7 +1,6 @@
 package com.lion;
 
 import com.lion.core.persistence.BaseDaoFactoryBean;
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication ()
 @ComponentScan(basePackages = "com.lion.**")
 @EnableDiscoveryClient
-@DubboComponentScan(basePackages = {"com.lion.**"})
 @EnableJpaRepositories(basePackages = {"com.lion.common.dao.**"}, repositoryFactoryBeanClass = BaseDaoFactoryBean.class)
 @EntityScan({"com.lion.common.entity.**"})
 @EnableJpaAuditing
